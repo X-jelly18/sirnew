b5d4fe63736e3cd84fa339cea0413cb1c6992b62
X-jelly18 <elitegamer128k@gmail.com>
2026-06-15 06:34:35 +0200

    Implement gRPC Relay service in index.ts

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+
+const packageDef = protoLoader.loadSync("relay.proto");
+const grpcObj: any = grpc.loadPackageDefinition(packageDef);
+
+const RelayService = grpcObj.relay.Relay.service;
+
+// your backend
+const BACKEND = "six.ayanakojivps.shop:80";
+
+function Stream(call: any) {
+  const backend = new grpcObj.relay.Relay(
+    BACKEND,
+    grpc.credentials.createInsecure()
+  );
+
+  const stream = backend.Stream();
+
+  call.on("data", (msg: any) => {
+    stream.write(msg);
+  });
+
+  stream.on("data", (msg: any) => {
+    call.write(msg);
+  });
+
+  call.on("end", () => {
+    stream.end();
+    call.end();
+  });
+
+  stream.on("error", () => call.end());
+}
+
+const server = new grpc.Server();
+
+server.addService(RelayService, { Stream });
+
+// IMPORTANT: Cloud Run uses HTTP/2 gRPC automatically
+server.bindAsync(
+  "0.0.0.0:8080",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    console.log("gRPC Relay running on Cloud Run");
+    server.start();
+  }
+);
